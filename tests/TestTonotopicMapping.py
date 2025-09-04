f1dd19fa5a9554693be02e6debea4cd141d5a4d2
Florian Rau <bimac@users.noreply.github.com>
2025-09-04 18:12:07 +0100

    add tests

---
+++ tests/TestTonotopicMapping.py
@@ -0,0 +1,58 @@
+import shutil
+import tempfile
+import unittest
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from pydantic import ValidationError
+
+from iblrig_custom_tasks.samuel_tonotopicMapping import task
+
+
+class TestCreateDataframe(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        """Create a temporary directory and copy the fixture file to it."""
+        cls.fixture = Path(__file__).parent / 'TonotopicMapping.jsonable'
+        assert cls.fixture.exists(), 'jsonable fixture not found'
+        cls.temp_dir = tempfile.TemporaryDirectory()
+        cls.jsonable = Path(cls.temp_dir.name) / '_iblrig_taskData.raw.jsonable'
+        shutil.copy(cls.fixture, cls.jsonable)
+        cls.dataframe = task.create_dataframe(cls.jsonable)
+
+    @classmethod
+    def tearDownClass(cls):
+        """Remove the temporary directory after the tests."""
+        cls.temp_dir.cleanup()
+
+    def test_file_not_found(self):
+        """Test that a non-existent file raises a ValidationError."""
+        with self.assertRaises(ValidationError):
+            task.create_dataframe('non_existent_file.jsonable')
+
+    def test_wrong_file_name(self):
+        """Test that a file with the wrong name raises a ValueError."""
+        with self.assertRaises(ValueError):
+            task.create_dataframe(self.fixture)
+
+    def test_return_type(self):
+        """Test that the function returns a pandas DataFrame."""
+        self.assertIsInstance(self.dataframe, pd.DataFrame)
+
+    def test_columns(self):
+        """Test that the DataFrame has the correct columns."""
+        self.assertListEqual(list(self.dataframe.columns), ['Trial', 'Stimulus', 'Value', 'Frequency', 'Attenuation'])
+
+    def test_index(self):
+        """Test that the DataFrame has a TimedeltaIndex."""
+        self.assertIsInstance(self.dataframe.index, pd.TimedeltaIndex)
+        self.assertEqual(self.dataframe.index.dtype, np.dtype('timedelta64[us]'))
+
+    def test_no_nans(self):
+        """Test that the DataFrame does not contain NaN values."""
+        self.assertTrue(all(~self.dataframe.isna().any()))
+
+
+if __name__ == '__main__':
+    unittest.main()
